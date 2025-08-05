f30ce57a442d88545a71d08259cbe8baecb5729b
Evanio Tech <evanioshark@gmail.com>
2025-08-05 08:49:43 -0300

    create button 'cadastrar' and 'login'

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function Home() {
   return (
@@ -7,6 +7,12 @@ export default function Home() {
       <View >
         <Image style={styles.image} source={require('../../assets/ger.png')} />
       </View>
+      <TouchableOpacity style={styles.buttonCad}>
+        <Text style={styles.buttonText}>Cadastrar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{marginTop: 10}}>
+        <Text style={{color: 'black'}}>Já tem cadastro? Faça login</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -24,4 +30,22 @@ const styles = StyleSheet.create({
     width: 350,
     height: 300,
   },
+    buttonCad: {
+        backgroundColor: '#007BFF',
+        padding: 15,
+        borderRadius: 5,
+        width: 300,
+        alignItems: 'center',
+        marginTop: 20,
+        
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 19,
+    },
+    buttonLog: {
+        color: '#fff',
+        fontSize: 16,
+    },
 });
