0e4e7e7a01fd763974f5b87ee302bcc671eb71a5
Evanio Tech <evanioshark@gmail.com>
2025-09-02 11:50:15 -0300

    update in layout

--- src/src/app/_layout.tsx
+++ src/src/app/_layout.tsx
@@ -1,11 +1,11 @@
 import { Stack } from "expo-router";
 
-
 export default function Layout() {
     return (
         <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="home" />
-            <Stack.Screen name="login" />
+            <Stack.Screen name="index" />
+            <Stack.Screen name="sign-in/index" />
+            <Stack.Screen name="sign-up/index" />
         </Stack>
     );
 }
